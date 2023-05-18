7919e2df2ac8802a0635f42b22156fe28a9d6631
rastelli-bernardita <rastellibernardita@gmail.com>
2023-05-17 23:48:18 -0300

    actualización

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function showAlert() {alert('¡Hola, esta es una alerta desde JavaScript!');}
+document.querySelector("button.button-menu-toggle")
+            .addEventListener("click", function() {
+                document.querySelector(".nav-links").
+                classList.toggle("nav-links-responsive")})
 const form = document.getElementById('myForm');
 form.addEventListener('submit', function(event) {
     event.preventDefault();
@@ -17,7 +21,3 @@ function validateForm() {
         alert('Correo electrónico enviado correctamente.');
     }
 }
-document.querySelector("button.button-menu-toggle")
-    .addEventListener("click", function() {
-        document.querySelector(".nav-links").
-        classList.toggle("nav-links-responsive")})
